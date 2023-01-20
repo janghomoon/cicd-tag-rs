11f17ae4f1188cdbd6dac93d4c3da012d57b69bb
jhm <jhmoon@howser.co.kr>
2023-01-20 17:04:28 +0900

    Initial commit

---
+++ src/main/java/com/test/cicdtagrs/CicdTagRsApplication.java
@@ -0,0 +1,14 @@
+package com.test.cicdtagrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTagRsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CicdTagRsApplication.class, args);
+
+  }
+
+}
